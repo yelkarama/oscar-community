87bb8cc7de0ce770e02bf4ed0e4e0227a6f035c5
John Blackwood <jb@kaiinnovations.com>
2016-07-27 13:43:55 -0400

    Updated KAI bar

--- src/main/webapp/js/custom/kai/main.js
+++ src/main/webapp/js/custom/kai/main.js
@@ -62,20 +62,20 @@ function addKAIBar() {
 		<a href="http://www.kaiinnovations.com" target="_blank"><img alt="" src="/oscar/js/custom/kai/KAI_LOGO2_HR.png" height="18" width="18">&nbsp;&nbsp;KAI INNOVATIONS</a>
 		<div class="block">
 			Search:
-			<input class="kaiInput" type="text" placeholder="Ender Heath Card # or Demographic Name"/>
-			<a href="" class="btnFlat">
-		    	<div class="white">
+			<input class="kaiInput" type="text" placeholder="Ender Heath Card # or Demographic Name" id="kaiDemoSearch"/>
+			<a href="" class="btnFlat" id='kaiDemoSearchButton'>
+		    	<div class="green">
 		            <span>Go</span>
 		        </div>
 		    </a>
 		</div>
 		<div class="block right">
-			<a href="" class="btnFlat">
+			<a href="https://oscarsupport.zendesk.com/hc/en-us" class="btnFlat" target="_blank">
 		    	<div class="green">
 		            <span>Help Portal</span>
 		        </div>
 		    </a>
-		    <a href="" class="btnFlat">
+		    <a href="" class="btnFlat" target="_blank">
 		    	<div class="green">
 		            <span>TeamViewer</span>
 		        </div>
@@ -87,7 +87,66 @@ function addKAIBar() {
   var kaiBar = jQuery(kaiBarHTML);
   kaiBar.insertAfter('table#firstTable');
   
+  var kaiDemoSearchButton = jQuery('#kaiDemoSearchButton');
+  kaiDemoSearchButton.click(kaiDemoSearch);
+}
 
+function getUrlVars(url) {
+  var vars = [], hash;
+  if(url === undefined) {
+    return null;
+  }
+  var hashes = url.toString().slice(url.toString().indexOf('?') + 1).split('&');
+  for(var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
+}
 
+function resolveCurrentProvider() {
+  // get the current provider
+  var providerNumber = null;
 
+  // check if pref is available
+  if (jQuery("[title*='Edit your personal setting']").length && jQuery("[title*='Edit your personal setting']").attr('onclick').length) {
+    var oldonclick = jQuery("[title*='Edit your personal setting']").attr('onclick');
+    providerNumber = oldonclick.toString().match('(?:provider_no=)([^&]*)')[1];
+  } else {
+    providerNumber = getUrlVars(jQuery("a:contains('Caseload')").attr("href"))["clProv"];
+  }
+  return providerNumber;
+}
+
+function kaiDemoSearch() {
+  var kaiDemoSearch = jQuery('#kaiDemoSearch');
+  var searchString = kaiDemoSearch.val();
+  var demoRegex = /^[A-Za-z].*$/;
+  if( demoRegex.test(searchString) ) {
+    var searchUrl = "/oscar/demographic/demographiccontrol.jsp?search_mode=search_name&keyword=" + encodeURIComponent(searchString) + "&orderby=last_name%2C+first_name&dboperation=search_titlename&limit1=0&limit2=10&displaymode=Search&ptstatus=active"
+    popupPage2(searchUrl);
+    kaiDemoSearch.val("");
+  } else {
+    // set the dimensions and open the popup window
+    var left = (screen.width / 2) - (500 / 2);
+    var top = (screen.height / 2) - (500 / 2);
+    // path to your card swipe module webapp must be specified. add it in here
+    var newwindow = window.open(selectedUrl + "/CardSwipeModule/?hc=" + escape(searchString) + "&providerNo=" + escape(resolveCurrentProvider()), "name",
+    "location=no,scrollbars=1,width=500,height=500,top=" + top + ",left=" + left);
+
+    // focus the window
+    if (window.focus) {
+      newwindow.focus();
+    }
+
+    var timer = setInterval(function() {
+      if (newwindow.closed) {
+        clearInterval(timer);
+        window.location.reload();
+      }
+    }, 100);
+  }
+  
+  return false;
 }
