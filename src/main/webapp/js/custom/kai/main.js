4bf54f2e30d8792fb5b3754bc891dd4528e54ef1
John Blackwood <jb@kaiinnovations.com>
2016-07-28 10:24:50 -0400

    Disabled new UI link

--- src/main/webapp/js/custom/kai/main.js
+++ src/main/webapp/js/custom/kai/main.js
@@ -51,6 +51,10 @@ jQuery(document).ready(function(){
 //	jQuery("#navlist").append("<li><a href=\"../eyeform/ConsultationReportList.do\">ConReport</a></li>");
 	jQuery("<li><a href=\"#\" onclick=\"popupOscarRx(625,1024,'../eyeform/ConsultationReportList.do\');\" title\"View Consultation Reports\">ConReport</a></li>").insertAfter("#con");
 
+  // Disable link to new UI
+  jQuery('a[href="../web/"]').attr("href",'#');
+  
+  // Add KAI bar as appropriate
   if (!SmartPhone.isMobile()) {
     addKAIBar();
   }
