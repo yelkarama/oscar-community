f4a497cd044cb70790cf53f8390c4f5b52c666de
Marc Dumontier <marc@mdumontier>
2018-10-16 11:29:05 -0400

    OSCAREMR-6579 SSO Login uses a Java 8 Base64 class

--- src/main/java/oscar/login/SSOLoginAction.java
+++ src/main/java/oscar/login/SSOLoginAction.java
@@ -39,6 +39,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
@@ -578,7 +579,8 @@ public final class SSOLoginAction extends MappingDispatchAction {
 	    try {
 	      String[] parts = data.split(":");
 
-	      IvParameterSpec iv = new IvParameterSpec(java.util.Base64.getDecoder().decode(parts[1]));
+	      IvParameterSpec iv = new IvParameterSpec( Base64.decodeBase64(parts[1]) );
+	  //    IvParameterSpec iv = new IvParameterSpec(java.util.Base64.getDecoder().decode(parts[1]));
 	      SecretKeySpec secretKey = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
 
 	      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
