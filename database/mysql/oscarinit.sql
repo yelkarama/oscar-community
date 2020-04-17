d9d0fd5eb6e19ac8198bde542d30a25dce0f3db3
Jason Gallagher <jaygallagher@gmail.com>
2020-04-17 01:41:07 -0400

    OSCAREMR-6963 Added ability to add dx links to appt screen

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -12477,3 +12477,18 @@ CREATE TABLE ISO36612 (
     country varchar(255),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE AppointmentDxLink (
+			id int(10)  NOT NULL auto_increment primary key,
+			providerNo varchar(6),
+			code varchar(20),
+			codeType varchar(20),
+			symbol varchar(20),
+			colour varchar(20),
+			message varchar(255),
+			link varchar(255),
+			updateDate datetime,
+			createDate datetime,
+			active boolean,
+			KEY(code)
+);
