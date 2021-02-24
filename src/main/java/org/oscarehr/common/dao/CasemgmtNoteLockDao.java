5d796846bab45ec8932165c484d9913f81ee3296
Peter H-C <phc@srpc.ca>
2021-02-24 23:17:25 +0000

    OSCAREMR-6988 added CasemgmtNoteLock isNoteLocked to CasemgmtNoteLockDao.java

--- src/main/java/org/oscarehr/common/dao/CasemgmtNoteLockDao.java
+++ src/main/java/org/oscarehr/common/dao/CasemgmtNoteLockDao.java
@@ -23,6 +23,8 @@
  */
 package org.oscarehr.common.dao;
 
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 import java.util.List;
 
 import javax.persistence.Query;
@@ -69,4 +71,13 @@ public class CasemgmtNoteLockDao extends AbstractDao<CasemgmtNoteLock> {
 		
 		return results;
 	}
+	
+	public CasemgmtNoteLock isNoteLocked(Long noteId) {
+		Query query = entityManager.createQuery("select lock from CasemgmtNoteLock lock where lock.noteId = :noteId");
+		query.setParameter("noteId", noteId);
+		query.setMaxResults(1);
+		List<CasemgmtNoteLock> results = query.getResultList();
+		return results.isEmpty() ? null : results.get(0);
+	}
+
 }
