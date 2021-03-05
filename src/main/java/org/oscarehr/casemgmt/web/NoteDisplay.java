0f84bf16026e92712448d0b0ee54c55e190c70e0
Peter H-C <phc@srpc.ca>
2021-03-05 09:33:45 +0000

    OSCAREMR-6988 reversion NoteDisplay.java

--- src/main/java/org/oscarehr/casemgmt/web/NoteDisplay.java
+++ src/main/java/org/oscarehr/casemgmt/web/NoteDisplay.java
@@ -134,14 +134,6 @@ public interface NoteDisplay {
 	//use this for a category of notes - like integrator, group notes, etc
 	public boolean isReadOnly();
 	
-	public CaseManagementNote getCaseManagementNote() {
-		return caseManagementNote;
-	}
-
-	public void setCaseManagementNote(CaseManagementNote caseManagementNote) {
-		this.caseManagementNote = caseManagementNote;
-	}
-
 	public boolean isGroupNote();
 
 	public boolean isCpp();
