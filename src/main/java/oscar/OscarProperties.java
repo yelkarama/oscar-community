452cd9bc0616b3677b34e5733e5812b73257ade4
Peter H-C <phc@srpc.ca>
2021-04-15 17:47:21 +0000

    OscarProperties.java edited online with Bitbucket

--- src/main/java/oscar/OscarProperties.java
+++ src/main/java/oscar/OscarProperties.java
@@ -33,8 +33,6 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
-import org.oscarehr.common.dao.SystemPreferencesDao;
-import org.oscarehr.util.SpringUtils;
 
 import org.oscarehr.util.MiscUtils;
 
