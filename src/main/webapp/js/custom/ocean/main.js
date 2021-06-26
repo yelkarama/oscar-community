7d2c263a8674b630b8329399b9504bf8ccfd5d43
Peter H-C <phc@srpc.ca>
2021-06-26 12:07:26 +0000

    main.js edited online with Bitbucket

--- src/main/webapp/js/custom/ocean/main.js
+++ src/main/webapp/js/custom/ocean/main.js
@@ -1,6 +1,7 @@
 //navlist
 jQuery(document).ready(function(){
-//	jQuery("#navlist").append("<li><a href=\"../eyeform/ConsultationReportList.do\">ConReport</a></li>");
-	jQuery("<li><a href=\"#\" onclick=\"popupOscarRx(625,1024,'../eyeform/ConsultationReportList.do\');\" title\"View Consultation Reports\">ConReport</a></li>").insertAfter("#con");
-	
+	var hide_ConReport = jQuery("#mainScript").attr('hide_ConReport') == "true";
+	if (!hide_ConReport) {
+		jQuery("<li><a href=\"#\" onclick=\"popupOscarRx(625,1024,'../eyeform/ConsultationReportList.do\');\" title\"View Consultation Reports\">ConReport</a></li>").insertAfter("#con");
+	}
 });
