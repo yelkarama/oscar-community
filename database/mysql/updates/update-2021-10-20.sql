5be6a153dd55da6606bdcbcfc52b2b661500d329
Peter H-C <phc@srpc.ca>
2021-11-16 21:32:57 +0000

    update-2021-10-20.sql fixed for checkstyle

--- database/mysql/updates/update-2021-10-20.sql
+++ database/mysql/updates/update-2021-10-20.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS `formBCAR2020` (
                                               `id` int(10) NOT NULL AUTO_INCREMENT,
                                               `demographic_no` int(10) NOT NULL,
                                               `provider_no` varchar(6) NOT NULL,
-                                              `formCreated` date DEFAULT NULL,
-                                              `formEdited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                              `formCreated` date,
+                                              `formEdited` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
                                               UNIQUE KEY `id` (`id`)
 );
 CREATE TABLE IF NOT EXISTS `formBCAR2020Data` (
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS `formBCAR2020Data` (
                                                   `provider_no` varchar(6) NOT NULL,
                                                   `page_no` int(1) NOT NULL,
                                                   `field` varchar(255) NOT NULL,
-                                                  `val` varchar(255) NOT NULL DEFAULT '',
-                                                  `field_edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                                  `val` varchar(255) NOT NULL,
+                                                  `field_edited` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
                                                   UNIQUE KEY `form_data` (`form_id`,`page_no`,`field`)
 );
 
@@ -22,8 +22,10 @@ CREATE TABLE IF NOT EXISTS `formBCAR2020Text` (
                                                   `page_no` int(1) NOT NULL,
                                                   `field` varchar(255) NOT NULL,
                                                   `val` text NOT NULL,
-                                                  `field_edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                                  `field_edited` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
                                                   UNIQUE KEY `form_data` (`form_id`,`page_no`,`field`)
 );
 
-INSERT INTO encounterForm VALUES ('BC-AR 2020', '../form/formBCAR2020pg1.jsp?demographic_no=', 'formBCAR2020', 1);
+INSERT INTO `encounterForm` VALUES ('BC-AR 2020', '../form/formBCAR2020pg1.jsp?demographic_no=', 'formBCAR2020', 1);
+
+ALTER TABLE `demographic` ADD `pref_name` VARCHAR(30) NOT NULL AFTER `consentToUseEmailForCare`;
