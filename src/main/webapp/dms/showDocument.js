016f84b3bdace42fcabc3c80992acdbf8b0a6622
Peter H-C <phc@srpc.ca>
2022-01-19 12:42:10 +0000

    showDocument.js added framework for opening inTabs

--- src/main/webapp/dms/showDocument.js
+++ src/main/webapp/dms/showDocument.js
@@ -1,21 +1,50 @@
-function popupPatient(height, width, url, windowName, docId) {
+function popupPatient(height, width, url, windowName, docId, inTabs) {
+      height      = typeof(height)    != 'undefined' ? height : '700px';
+      width       = typeof(width)     != 'undefined' ? width : '1024px';
+      url         = typeof(url)       != 'undefined' ? url : '';
+      windowName  = typeof(windowName)!= 'undefined' ? windowName : 'demoEdit';
+      docId       = typeof(docId)     != 'undefined' ? docId : '0';
+      inTabs      = typeof(inTabs)    != 'undefined' ? inTabs : false;
 	  d = document.getElementById('demofind'+ docId).value; //demog  //attachedDemoNo
 	  urlNew = url + d;
-	
-	  return popup2(height, width, 0, 0, urlNew, windowName);
+      if (inTabs) {
+	        return window.open(urlNew, windowName);
+        } else {
+	        return popup2(height, width, 0, 0, urlNew, windowName);
+      }
 }
 
-function popupPatientTicklerPlus(height, width, url, windowName,docId) {
-  d = document.getElementById('demofind'+ docId).value; //demog  //attachedDemoNo
-  n = document.getElementById('demofindName' + docId).value;
-  urlNew = url + "method=edit&tickler.demographic_webName=" + n + "&tickler.demographicNo=" +  d + "&docType=DOC&docId="+docId;
-  	
-  	  return popup2(height, width, 0, 0, urlNew, windowName);
+function popupPatientTicklerPlus(height, width, url, windowName, docId, inTabs) {
+      height      = typeof(height)    != 'undefined' ? height : '700px';
+      width       = typeof(width)     != 'undefined' ? width : '1024px';
+      url         = typeof(url)       != 'undefined' ? url : '';
+      windowName  = typeof(windowName)!= 'undefined' ? windowName : 'demoEdit';
+      docId       = typeof(docId)     != 'undefined' ? docId : '0';
+      inTabs      = typeof(inTabs)    != 'undefined' ? inTabs : false;
+      d = document.getElementById('demofind'+ docId).value; //demog  //attachedDemoNo
+      n = document.getElementById('demofindName' + docId).value;
+      urlNew = url + "method=edit&tickler.demographic_webName=" + n + "&tickler.demographicNo=" +  d + "&docType=DOC&docId="+docId;
+      if (inTabs) {
+	        return window.open(urlNew, windowName);
+        } else {
+	        return popup2(height, width, 0, 0, urlNew, windowName);
+      }
 }
 
-function popupPatientTickler(height, width, url, windowName,docId) {
+function popupPatientTickler(height, width, url, windowName, docId, inTabs) {
+      height      = typeof(height)    != 'undefined' ? height : '700px';
+      width       = typeof(width)     != 'undefined' ? width : '1024px';
+      url         = typeof(url)       != 'undefined' ? url : '';
+      windowName  = typeof(windowName)!= 'undefined' ? windowName : 'demoEdit';
+      docId       = typeof(docId)     != 'undefined' ? docId : '0';
+      inTabs      = typeof(inTabs)    != 'undefined' ? inTabs : false;
 	  d = document.getElementById('demofind'+ docId).value; //demog  //attachedDemoNo
 	  n = document.getElementById('demofindName' + docId).value;
 	  urlNew = url + "demographic_no="+d+"&name="+n+"&chart_no=&bFirstDisp=false&messageID=null&remoteFacilityId=&docType=DOC&docId="+docId;
-	  return popup2(height, width, 0, 0, urlNew, windowName);
+      if (inTabs) {
+	        return window.open(urlNew, windowName);
+        } else {
+	        return popup2(height, width, 0, 0, urlNew, windowName);
+      }
 }
+
