770606b8fc09ed709057f60bfb4b6403c3b033a3
peterHC <phc@srpc.ca>
2022-03-08 16:36:15 -0500

    add in a try catch

--- src/main/java/oscar/login/LoginCheckLoginBean.java
+++ src/main/java/oscar/login/LoginCheckLoginBean.java
@@ -93,14 +93,14 @@ public final class LoginCheckLoginBean {
 			Integer numDigits = security.getTotpDigits();
 			try {
 				String code = TimeBasedOneTimePasswordUtil.generateCurrentNumberString(base32Secret, numDigits);
+				if (isWAN() && security.getBRemotelockset() != null && security.getBRemotelockset().intValue() == 1 && (!sPin.equals(code) || pin.length() < 3)) {
+					return cleanNullObj(LOG_PRE + "Pin-remote 2FA needed: " + username);
+				} else if (!isWAN() && security.getBLocallockset() != null && security.getBLocallockset().intValue() == 1 && (!sPin.equals(code) || pin.length() < 3)) {
+					return cleanNullObj(LOG_PRE + "Pin-local 2FA needed: " + username);
+				}	
 			} catch (Exception e) {
 				logger.error(e.getMessage());
 			}
-			if (isWAN() && security.getBRemotelockset() != null && security.getBRemotelockset().intValue() == 1 && (!sPin.equals(code) || pin.length() < 3)) {
-				return cleanNullObj(LOG_PRE + "Pin-remote 2FA needed: " + username);
-			} else if (!isWAN() && security.getBLocallockset() != null && security.getBLocallockset().intValue() == 1 && (!sPin.equals(code) || pin.length() < 3)) {
-				return cleanNullObj(LOG_PRE + "Pin-local 2FA needed: " + username);
-			}		
 		} else {
 			if (isWAN() && security.getBRemotelockset() != null && security.getBRemotelockset().intValue() == 1 && (!sPin.equals(security.getPin()) || pin.length() < 3)) {
 				return cleanNullObj(LOG_PRE + "Pin-remote needed: " + username);
