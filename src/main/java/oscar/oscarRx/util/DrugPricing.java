9a4fee45e93a605718f9e51cf5b2de4b0dd47e2c
Peter H-C <phc@srpc.ca>
2022-04-10 21:54:43 +0000

    DrugPricing.java removed unused import

--- src/main/java/oscar/oscarRx/util/DrugPricing.java
+++ src/main/java/oscar/oscarRx/util/DrugPricing.java
@@ -29,7 +29,6 @@ import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Hashmap;
 import java.util.Iterator;
 
