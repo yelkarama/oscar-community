c0f23bf3e35b60231fb1630abcd718341ea3e31d
Peter H-C <phc@srpc.ca>
2022-05-02 13:09:54 +0000

    mouseover.js

--- src/main/webapp/share/javascript/mouseover.js
+++ src/main/webapp/share/javascript/mouseover.js
@@ -16,16 +16,18 @@ if (window.navigator.userAgent.toLowerCase().match("gecko")) {
 if (navigator.userAgent.toLowerCase().indexOf('safari') > -1) {
     navigator.family = "safari";
 }
-
+let mouseOverX;
+let mouseOverY;
 var overdiv="0";
 //  #########  CREATES POP UP BOXES 
 function popLayer(content){
     
     if (navigator.family == "gecko") {
-        pad="0"; bord="1 bordercolor=black";
-    }
-    else {
-        pad="1"; bord="0";
+        pad="0"; 
+        bord="1 bordercolor=black";
+    } else {
+        pad="1";
+        bord="0";
     }
 
     desc = "<table cellspacing=0 cellpadding="+pad+" border="+bord+"  bgcolor=000000><tr><td>\n"
@@ -34,33 +36,29 @@ function popLayer(content){
 	+"\n</td></tr></table>\n"
 	+"</td></tr></table>";
 
-    if(navigator.family =="nn4") {
-	document.object1.document.write(desc);
-	document.object1.document.close();
-	document.object1.left=x+15;
-	document.object1.top=y-5;
-    }
-    else if( navigator.family == "safari" ){
-	object1.innerHTML=desc;
-	object1.style.pixelLeft=x+15;
-	object1.style.pixelTop=y-5;
-    }
-    else if(navigator.family =="ie4" || navigator.family =="gecko"){
-	document.getElementById("object1").innerHTML=desc;
-	document.getElementById("object1").style.left=x+15;
-	document.getElementById("object1").style.top=y-5;
+    if (navigator.family =="nn4") {
+        document.object1.document.write(desc);
+        document.object1.document.close();
+        document.object1.left = mouseOverX + 15;
+        document.object1.top = mouseOverY - 5;
+    } else if(navigator.family == "safari" ) {
+        document.getElementById("object1").innerHTML = desc;
+        document.getElementById("object1").style.pixelLeft = mouseOverX + 15;
+        document.getElementById("object1").style.pixelTop = mouseOverY - 5;
+    } else if (navigator.family =="ie4" || navigator.family =="gecko") {
+        document.getElementById("object1").innerHTML = desc;
+        document.getElementById("object1").style.left = mouseOverX + 15;
+        document.getElementById("object1").style.top = mouseOverY - 5;
     }
 }
 
 function hideLayer(){
     if (overdiv == "0") {
-	if(navigator.family =="nn4") {
+        if (navigator.family =="nn4") {
             eval(document.object1.top="-500");
-        }
-	else if( navigator.family == "safari" ){
-            object1.innerHTML="";
-        }
-	else if(navigator.family =="ie4" || navigator.family =="gecko") {
+        } else if (navigator.family == "safari") {
+                object1.innerHTML="";
+        } else if(navigator.family =="ie4" || navigator.family =="gecko") {
             document.getElementById("object1").style.top="-500";
         }
     }
@@ -69,13 +67,13 @@ function hideLayer(){
 //  ########  TRACKS MOUSE POSITION FOR POPUP PLACEMENT
 
 function handlerMM(e){
-    x = (isNav) ? e.pageX : event.clientX + document.body.scrollLeft;
-    y = (isNav) ? e.pageY : event.clientY + document.body.scrollTop;
+    mouseOverX = (isNav) ? e.pageX : event.clientX + document.body.scrollLeft;
+    mouseOverY = (isNav) ? e.pageY : event.clientY + document.body.scrollTop;
 }
 
 
 var isNav = (navigator.appName.indexOf("Netscape") !=-1);
-if (isNav){
+if (isNav) {
     document.captureEvents(Event.MOUSEMOVE);
 }
 
