936603f445c2b4ab5fcfc4152d6eb0dd2a781cf5
Peter H-C <phc@srpc.ca>
2024-09-07 12:56:01 +0000

    PrintAction.java de

--- src/main/java/oscar/eform/actions/PrintAction.java
+++ src/main/java/oscar/eform/actions/PrintAction.java
@@ -152,7 +152,7 @@ public class PrintAction extends Action {
 						ImagePDFCreator ipdfc = new ImagePDFCreator(request, bos);
 						try {
 							ipdfc.printPdf();
-						} catch(com.lowagie.text.DocumentException de) {
+						} catch(com.itextpdf.text.DocumentException de) {
 							logger.error("PrintAction : " +de);
 						}
 						
