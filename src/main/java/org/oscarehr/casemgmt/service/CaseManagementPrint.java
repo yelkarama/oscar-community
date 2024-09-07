46a98c6b4618de9901b1ba42a3f0278da26490eb
Peter H-C <phc@srpc.ca>
2024-09-07 13:22:22 +0000

    CaseManagementPrint.java added Document Exception

--- src/main/java/org/oscarehr/casemgmt/service/CaseManagementPrint.java
+++ src/main/java/org/oscarehr/casemgmt/service/CaseManagementPrint.java
@@ -103,7 +103,7 @@ public class CaseManagementPrint {
         String[] noteIds, boolean printCPP, boolean printRx, boolean printLabs,
         boolean printPreventions, boolean printMeasurements, boolean printDocuments,
         boolean printHrms, Calendar startDate, Calendar endDate, HttpServletRequest request,
-        OutputStream os) throws IOException, DocumentException { 
+        OutputStream os) throws IOException, com.lowagie.text.DocumentException, com.itextpdf.text.DocumentException { 
 			
 		String providerNo=loggedInInfo.getLoggedInProviderNo();
 
