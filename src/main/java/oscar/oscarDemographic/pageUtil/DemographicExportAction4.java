108f4329ac9cb4578a3c570544674f83086a53d6
Peter H-C <phc@srpc.ca>
2025-06-07 21:24:49 +0000

    DemographicExportAction4.java }

--- src/main/java/oscar/oscarDemographic/pageUtil/DemographicExportAction4.java
+++ src/main/java/oscar/oscarDemographic/pageUtil/DemographicExportAction4.java
@@ -2476,7 +2476,7 @@ public class DemographicExportAction4 extends Action {
 					throw new Exception("Cannot write .xml file(s) to export directory.\n Please check directory permissions.");
 		}
 	}
-
+} 
 	// Validate export against xsd
 	for (File f: files) {
 		Boolean valid = validateExport(f);
