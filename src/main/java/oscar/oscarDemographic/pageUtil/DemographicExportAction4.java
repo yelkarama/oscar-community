4657cc8c9fa0f3371407eafa5f3d67ce72e8a54f
Peter H-C <phc@srpc.ca>
2025-06-08 00:46:31 +0000

    Merged OSCAREMR-7262-export-fails into stable
    * DemographicExportAction4.java added try catch to allow for partial exports
    
    * DemographicExportAction4.java
    
    * DemographicExportAction4.java }
    
    * DemographicExportAction4.java demoNo
    
    * DemographicExportAction4.java 			org.oscarehr.common.model.Demographic demographic = null;
        			try {
        				demographic = d.getDemographic(LoggedInInfo.getLoggedInInfoFromSession(request), demoNo);
        			}catch(PatientDirectiveException e) {
        				exportError.add("Unable to export patient " + demoNo + " due to Patient Directive");
        				continue;
        			}
    
    * DemographicExportAction4.java DemographicData d

--- src/main/java/oscar/oscarDemographic/pageUtil/DemographicExportAction4.java
+++ src/main/java/oscar/oscarDemographic/pageUtil/DemographicExportAction4.java
@@ -56,6 +56,7 @@ import javax.xml.validation.Validator;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.commons.lang.WordUtils;
 import org.apache.commons.lang.math.NumberUtils;
@@ -328,6 +329,10 @@ public class DemographicExportAction4 extends Action {
 		ArrayList<String> dirs = new ArrayList<String>();
 		exportError = new ArrayList<String>();
 		for (String demoNo : list) {
+		
+        String exceptionString = null;
+		OmdCdsDocument omdCdsDoc = OmdCdsDocument.Factory.newInstance();
+        try {
 			if (StringUtils.empty(demoNo)) {
 				exportError.add("Error! No Demographic Number");
 				continue;
@@ -352,7 +357,7 @@ public class DemographicExportAction4 extends Action {
 			HashMap<String,String> demoExt = new HashMap<String,String>();
 			demoExt.putAll(demographicExtDao.getAllValuesForDemo(Integer.parseInt(demoNo)));
 
-			OmdCdsDocument omdCdsDoc = OmdCdsDocument.Factory.newInstance();
+			
 			OmdCdsDocument.OmdCds omdCds = omdCdsDoc.addNewOmdCds();
 			PatientRecord patientRec = omdCds.addNewPatientRecord();
 			Demographics demo = patientRec.addNewDemographics();
@@ -2446,6 +2451,12 @@ public class DemographicExportAction4 extends Action {
 			}
 			exportNo++;
 
+            } catch (Exception e) {
+                logger.error ("Error: Unable to export patient " + demoNo + " due to an unexpected error. This demographic has been skipped.", e);
+                exportError.add("Error: Unable to export patient " + demoNo + " due to an unexpected error. This demographic has been skipped.");
+                exceptionString = ExceptionUtils.getStackTrace(e);
+            } finally {
+
 
 			//export file to temp directory
 			try{
@@ -2454,6 +2465,10 @@ public class DemographicExportAction4 extends Action {
 					//this would never happen
 					throw new Exception("Temporary Export Directory does not exist!");
 				}
+                
+                DemographicData d = new DemographicData();
+                org.oscarehr.common.model.Demographic demographic = null;
+                demographic = d.getDemographic(LoggedInInfo.getLoggedInInfoFromSession(request), demoNo);
 
 				//Standard format for xml exported file : PatientFN_PatientLN_PatientUniqueID_DOB (DOB: ddmmyyyy)
 				String expFile = demographic.getFirstName()+"_"+demographic.getLastName();
@@ -2476,6 +2491,7 @@ public class DemographicExportAction4 extends Action {
 					throw new Exception("Cannot write .xml file(s) to export directory.\n Please check directory permissions.");
 		}
 	}
+    }
 	// Validate export against xsd
 	for (File f: files) {
 		Boolean valid = validateExport(f);
